4e237f60da2690881605fba1728b81cee254e8df
Aayush Senapati <aayushsenapati2002@gmail.com>
2024-04-14 10:23:15 +0000

    format main.cpp and add tls code

--- src/main.cpp
+++ src/main.cpp
@@ -16,10 +16,33 @@ void task2(int x, int y, std::string z)
     pthread_mutex_unlock(&cout_lock);
 }
 
+// Function that each thread will run
+void* threadFunc(void* arg) {
+    // Cast the argument to ThreadLocalStorage<int>*
+    Threading::ThreadLocalStorage<pthread_t>* tls = static_cast<Threading::ThreadLocalStorage<pthread_t>*>(arg);
+
+    // Set a thread-local variable
+    tls->set(pthread_self());
+
+    // Get the thread-local variable
+    pthread_t value = tls->get();
+
+    // Print the thread ID and the thread-local variable
+    std::cout << "Thread ID: " << pthread_self() << ", Thread-Local Variable: " << value << std::endl;
+
+    return nullptr;
+}
+
 int main()
 {
     try
     {
+
+
+/*-----------------------------------------------------------------------------------------------------------------------------------------------------------------------------*/
+        std::cout << "Testing Threadpool" << std::endl;
+
+
         pthread_mutex_init(&cout_lock, NULL);
         Threading::ThreadPool<4> pool;
         pool.enqueue(task1);
@@ -31,12 +54,21 @@ int main()
                 pthread_mutex_unlock(&cout_lock);
             });
 
+        Threading::Master master;
+        master.stopThreadPool(pool);
+        std::cout << "*-------COMPLETED----------------------------------------------------------------------------------------------------------*" << std::endl;
+
+/*-----------------------------------------------------------------------------------------------------------------------------------------------------------------------------*/
+        std::cout << "Testing Parallel For" << std::endl;
+
         int x = 5;
         Threading::parallel_for_pthreads(0, 10, [x](int i)
                                          { std::cout << "Task " << i << " finished by thread " << pthread_self() << ", x = " << x << std::endl; });
 
-        Threading::Master master;
-        master.stopThreadPool(pool);
+        std::cout << "*-------COMPLETED----------------------------------------------------------------------------------------------------------*" << std::endl;
+
+/*-----------------------------------------------------------------------------------------------------------------------------------------------------------------------------*/
+        std::cout << "Testing Mutex and Lock" << std::endl;
 
         Threading::Mutex mtx;
 
@@ -50,6 +82,24 @@ int main()
         }
 
         std::cout << "Outside critical section.\n";
+
+        std::cout << "*-------COMPLETED----------------------------------------------------------------------------------------------------------*" << std::endl;
+/*-----------------------------------------------------------------------------------------------------------------------------------------------------------------------------*/
+        std::cout << "Testing Thread Local Storage" << std::endl;
+        // Create a ThreadLocalStorage instance
+        Threading::ThreadLocalStorage<pthread_t> tls;
+
+        // Create two threads
+        pthread_t thread1, thread2;
+        pthread_create(&thread1, nullptr, threadFunc, &tls);
+        pthread_create(&thread2, nullptr, threadFunc, &tls);
+
+        // Wait for the threads to finish
+        pthread_join(thread1, nullptr);
+        pthread_join(thread2, nullptr);
+
+        std::cout << "*-------COMPLETED----------------------------------------------------------------------------------------------------------*" << std::endl;
+/*-----------------------------------------------------------------------------------------------------------------------------------------------------------------------------*/
         return 0;
     }
     catch (const std::exception &e)
